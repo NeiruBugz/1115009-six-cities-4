68a484035c72cbd7852ecbbece2661bf5f2f7363
Nail Badiullin <badiullin.nail@gmail.com>
2020-06-09 12:00:52 +0400

    remove default prop value

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,7 +1,7 @@
 import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
 
-const Main = ({rentOffers = 312}) => {
+const Main = ({rentOffers}) => {
   return <Fragment>
     <div style={{display: `none`}}>
       <svg xmlns="http://www.w3.org/2000/svg">
