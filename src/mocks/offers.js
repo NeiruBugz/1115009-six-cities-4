e0777f98f251e6b15fb2b4f77ad1f1ef86139d5d
Nail Badiullin <badiullin.nail@gmail.com>
2020-06-19 15:06:29 +0400

    [module3-task1] add mocks

---
+++ src/mocks/offers.js
@@ -0,0 +1,60 @@
+const offers = [
+  {
+    id: 0,
+    mark: `Premium`,
+    image: `img/apartment-01.jpg`,
+    price: 120,
+    priceText: `night`,
+    rating: 80,
+    type: `Apartment`,
+  },
+  {
+    id: 1,
+    mark: `Premium`,
+    image: `img/apartment-01.jpg`,
+    price: 120,
+    priceText: `night`,
+    rating: 80,
+    type: `Apartment`,
+  },
+  {
+    id: 3,
+    mark: `Premium`,
+    image: `img/apartment-01.jpg`,
+    price: 120,
+    priceText: `night`,
+    rating: 80,
+    type: `Apartment`,
+  },
+  {
+    id: 4,
+    mark: `Premium`,
+    image: `img/apartment-01.jpg`,
+    price: 120,
+    priceText: `night`,
+    rating: 80,
+    type: `Apartment`,
+  },
+  {
+    id: 5,
+    mark: `Premium`,
+    image: `img/apartment-01.jpg`,
+    price: 120,
+    priceText: `night`,
+    rating: 80,
+    type: `Apartment`,
+  },
+  {
+    id: 6,
+    mark: `Premium`,
+    image: `img/apartment-01.jpg`,
+    price: 120,
+    priceText: `night`,
+    rating: 80,
+    type: `Apartment`,
+  }
+];
+
+export default {
+  offers,
+};
