8eaffccb462a6b4594b4943ab2787e0a299f094b
Nail Badiullin <badiullin.nail@gmail.com>
2020-07-16 21:18:02 +0400

    6.1.2 fix sort

--- src/shared/utils/functions.js
+++ src/shared/utils/functions.js
@@ -1,19 +1,18 @@
 import {SORT_TYPES} from './consts';
 
 export const sortOffers = (offers, sortType) => {
-  let newOffers = [];
+  let newOffers = [...offers];
   switch (sortType) {
     case SORT_TYPES.TOP_RATED_FIRST:
-      newOffers = offers.sort((a, b) => b.rating - a.rating);
+      newOffers.sort((a, b) => b.rating - a.rating);
       break;
     case SORT_TYPES.PRICE_LOW_TO_HIGH:
-      newOffers = offers.sort((a, b) => a.price - b.price);
+      newOffers.sort((a, b) => a.price - b.price);
       break;
     case SORT_TYPES.PRICE_HIGH_TO_LOW:
-      newOffers = offers.sort((a, b) => b.price - a.price);
+      newOffers.sort((a, b) => b.price - a.price);
       break;
     default:
-      newOffers = offers;
       break;
   }
 
