ce3e993599daf9feca8fe45b09107f11ed4a89d6
Nail Badiullin <badiullin.nail@gmail.com>
2020-07-18 16:37:50 +0400

    [typescript] rewrite mocks

--- src/mocks/reviews.ts
+++ src/mocks/reviews.ts
@@ -1,26 +1,40 @@
-export const reviews = [
+import { ReviewType } from "../types/review.types";
+
+export const reviews: ReviewType[] = [
   {
     id: 0,
-    userAvatar: `img/avatar-max.jpg`,
-    userName: `Max`,
     rating: 80,
-    text: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
+    comment: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`,
     date: `April 2019`,
+    user: {
+      avatar_url: `img/avatar-max.jpg`,
+      is_pro: false,
+      id: 0,
+      name: `Max`
+    }
   },
   {
     id: 1,
-    userAvatar: `img/avatar-max.jpg`,
-    userName: `Jack`,
     rating: 20,
-    text: `Terrible!!!!!!!!`,
+    comment: `Terrible!!!!!!!!`,
     date: `April 2019`,
+    user: {
+      avatar_url: `img/avatar-max.jpg`,
+      is_pro: false,
+      id: 0,
+      name: `Jack`
+    }
   },
   {
     id: 2,
-    userAvatar: `img/avatar-max.jpg`,
-    userName: `Mellany`,
     rating: 100,
-    text: `I've got some good times with this place. Lovely`,
+    comment: `I've got some good times with this place. Lovely`,
     date: `March 2019`,
+    user: {
+      avatar_url: `img/avatar-max.jpg`,
+      is_pro: false,
+      id: 0,
+      name: `Mellany`
+    }
   }
 ];
